fdf2455280ef15c6a262f1ef436a4f2119dfb2ce
Andrey <ozoregl@gmail.com>
2024-05-04 21:10:49 +0300

    refactoring

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 
 public class Even {
     public static void start() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        var userName = Cli.greetUser();
         var streak = 0;
         while (streak < 3) {
             var randomNumber = (int) ((Math.random() * (100 - 1)) + 1);
             System.out.println("Question: " + randomNumber);
             System.out.print("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
             String userAnswer = scanner.next().toLowerCase();
 
             String correctAnswer = randomNumber % 2 == 0 ? "yes" : "no";
