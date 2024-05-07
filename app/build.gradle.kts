8962f6ab1eddb98f76fc1d60351fce202141efd0
Andrey <ozoregl@gmail.com>
2024-05-07 07:58:50 +0300

    additional files

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    application
+    distribution
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
